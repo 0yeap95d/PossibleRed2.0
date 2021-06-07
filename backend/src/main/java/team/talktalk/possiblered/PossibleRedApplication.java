fdf7d6401395dbf3afba9c50cad3ccd160d81a93
0yeap95d <tjdwob@gmail.com>
2021-06-07 14:55:05 +0900

    Init Project | 배성재

---
+++ backend/src/main/java/team/talktalk/possiblered/PossibleRedApplication.java
@@ -0,0 +1,13 @@
+package team.talktalk.possiblered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PossibleRedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PossibleRedApplication.class, args);
+    }
+
+}
