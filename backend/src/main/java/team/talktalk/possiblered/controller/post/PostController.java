18ac4ec9d319a13ca56dfb3c6e035c4c4772f702
jaehyeok <hyeok9sae@gmail.com>
2021-06-08 21:27:18 +0900

    test

--- backend/src/main/java/team/talktalk/possiblered/controller/post/PostController.java
+++ backend/src/main/java/team/talktalk/possiblered/controller/post/PostController.java
@@ -2,8 +2,17 @@ package team.talktalk.possiblered.controller.post;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team.talktalk.possiblered.dto.post.PostResponseDto;
+import team.talktalk.possiblered.dto.post.PostSaveRequestDto;
+import team.talktalk.possiblered.dto.post.PostUpdateRequestDto;
+import team.talktalk.possiblered.service.post.PostService;
+
+import javax.persistence.PostUpdate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/post")
@@ -14,4 +23,37 @@ public class PostController {
 
     // 포스트 - 목록 조회
     @ApiOperation(value = "포스트 목록 조회 API", notes = "포스트 목록 리스트 반환")
+    @GetMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<PostResponseDto>> findAll(){
+        List<PostResponseDto> postResponseDtoList = postService.findAll();
+        return new ResponseEntity<List<PostResponseDto>>(postResponseDtoList, HttpStatus.OK);
+    }
+    // 포스트 - 상세 조회
+    @ApiOperation(value = "포스트 상세 조회 API", notes = "포스트 상세 정보 반환")
+    @GetMapping(value = "/{postNo}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<PostResponseDto> findById(@PathVariable("postNo") Long postNo){
+        PostResponseDto postResponseDto = postService.findById(postNo);
+        return new ResponseEntity<PostResponseDto>(postResponseDto, HttpStatus.OK);
+    }
+    // 포스트 - 등록
+    @ApiOperation(value = "포스트 등록 API", notes = "포스트 등록 후 해당 id값 반환")
+    @PostMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Long> save(@RequestBody PostSaveRequestDto postSaveRequestDto){
+        Long savedPostId = postService.save(postSaveRequestDto);
+        return new ResponseEntity<Long>(savedPostId, HttpStatus.CREATED);
+    }
+    // 포스트 - 수정
+    @ApiOperation(value = "포스트 수정 API", notes = "포스트 수정 후 해당 id값 반환")
+    @PutMapping(value = "/{postNo}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Long> update(@PathVariable("postNo") Long postNo, @RequestBody PostUpdateRequestDto postUpdateRequestDto){
+        Long updatedPostId = postService.update(postNo, postUpdateRequestDto);
+        return new ResponseEntity<Long>(updatedPostId, HttpStatus.CREATED);
+    }
+    // 포스트 - 삭제
+    @ApiOperation(value = "포스트 삭제 API", notes = "포스트 삭제 후 해당 id값 반환")
+    @DeleteMapping(value = "/{postNo}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Long> delete(@PathVariable("postNo") Long postNo){
+        postService.delete(postNo);
+        return new ResponseEntity<Long>(postNo, HttpStatus.NO_CONTENT);
+    }
 }
--- backend/src/main/java/team/talktalk/possiblered/service/post/PostService.java
+++ backend/src/main/java/team/talktalk/possiblered/service/post/PostService.java
@@ -3,6 +3,9 @@ package team.talktalk.possiblered.service.post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team.talktalk.possiblered.dto.post.PostResponseDto;
+import team.talktalk.possiblered.dto.post.PostSaveRequestDto;
+import team.talktalk.possiblered.dto.post.PostUpdateRequestDto;
+import team.talktalk.possiblered.entity.post.Post;
 import team.talktalk.possiblered.repo.post.PostRepository;
 
 import javax.transaction.Transactional;
@@ -24,5 +27,30 @@ public class PostService {
     }
     // 포스트 - 상세 조회
     @Transactional
-    public PostResponseDto findByNo(Long postNo)
+    public PostResponseDto findById(Long postNo){
+        Post post = postRepository.findById(postNo)
+                .orElseThrow(() -> new IllegalAccessError("[postNo =" + postNo + "] 해당 포스트가 존재하지 않습니다."));
+        return new PostResponseDto(post);
+    }
+    // 포스트 - 등록
+    @Transactional
+    public Long save(PostSaveRequestDto postSaveRequestDto){
+        return postRepository.save(postSaveRequestDto.toEntity())
+                .getPostNo();
+    }
+    // 포스트 - 수정
+    @Transactional
+    public Long update(Long postNo, PostUpdateRequestDto postUpdateRequestDto){
+        Post post = postRepository.findById(postNo)
+                .orElseThrow(() -> new IllegalAccessError("[postNo=" + postNo + "] 해당 포스트가 존재하지 않습니다."));
+        post.update(postUpdateRequestDto.getPostDate(), postUpdateRequestDto.getPostPhoto(), postUpdateRequestDto.getPostContent());
+        return postNo;
+    }
+    // 포스트 - 삭제
+    @Transactional
+    public void delete(Long postNo){
+        Post post = postRepository.findById(postNo)
+                .orElseThrow(() -> new IllegalAccessError("[postNo=" + postNo + "] 해당 포스트가 존재하지 않습니다."));
+        postRepository.delete(post);
+    }
 }
